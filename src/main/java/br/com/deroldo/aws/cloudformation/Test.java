20876eb0e6b6a1401a8ffce37d1a0d1db249ed93
Diogo Fernandes <diogo@deroldo.com.br>
2018-02-09 01:35:48 -0200

    fazendo o replace na classe test

--- src/main/java/br/com/deroldo/aws/cloudformation/Test.java
+++ src/main/java/br/com/deroldo/aws/cloudformation/Test.java
@@ -2,6 +2,11 @@ package br.com.deroldo.aws.cloudformation;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,61 +25,154 @@ public class Test {
     public static void main (final String[] args) throws IOException {
         final JsonObject json = getJsonObject("teste");
 
+        // aws object
+        JsonObject awsJsonObject = new JsonObject();
+        awsJsonObject.add("Resources", new JsonObject());
+        awsJsonObject.add("Outputs", new JsonObject());
+        awsJsonObject.add("Mappings", new JsonObject());
+
+        // aux variable to index resources, outputs and mappings names to always be different
+        final AtomicInteger index = new AtomicInteger(1);
+
         for (final String userResourceName : json.keySet()) {
             final JsonObject userResource = json.get(userResourceName).getAsJsonObject();
             final String templateName = userResource.get("Template").getAsString();
             final JsonObject template = getJsonObject(templateName);
-            for (final String userResourceParamName : userResource.keySet()) {
-                replaceParam(userResourceParamName, userResource.get(userResourceParamName), template);
-            }
+
+            // replace with user params
+            userResource.keySet().forEach(userResourceParamName -> replaceParam(userResourceParamName, userResource.get(userResourceParamName), template, new ArrayList<>()));
+
+            // replace with defaults params
+            JsonObject parameters = template.get("Parameters").getAsJsonObject();
+            parameters.getAsJsonObject().keySet().stream()
+                    .filter(templateResourceParamName -> Objects.nonNull(parameters.get(templateResourceParamName).getAsJsonObject().get("Default")))
+                    .forEach(templateResourceParamName -> replaceParam(templateResourceParamName, parameters.get(templateResourceParamName).getAsJsonObject().get("Default"), template, new ArrayList<>()));
+
+            // adding resources with params replaced on aws object
+            template.get("Resources").getAsJsonObject().entrySet().forEach(set -> awsJsonObject.get("Resources").getAsJsonObject().add(set.getKey() + index.get(), set.getValue()));
+
+            // adding outputs with params replaced on aws object
+            Optional.ofNullable(template.get("Outputs")).ifPresent(outputs -> outputs.getAsJsonObject().entrySet().forEach(set -> {
+                awsJsonObject.get("Outputs").getAsJsonObject().add(set.getKey() + index.get(), set.getValue());
+            }));
+
+            // adding mappings with params replaced on aws object
+            Optional.ofNullable(template.get("Mappings")).ifPresent(outputs -> outputs.getAsJsonObject().entrySet().forEach(set -> {
+                awsJsonObject.get("Mappings").getAsJsonObject().add(set.getKey() + index.get(), set.getValue());
+            }));
+
+            index.incrementAndGet();
         }
+
+        // getting aws yml
+        String awsYml = getAwsYml(awsJsonObject);
+
+        System.out.println(awsYml);
+    }
+
+    private static String getAwsYml(JsonObject awsJsonObject) throws IOException {
+        String awsJson = GSON.toJson(awsJsonObject);
+        JsonNode awsJsonNode = JSON_MAPPER.readValue(awsJson, JsonNode.class);
+        return YML_MAPPER.writeValueAsString(awsJsonNode);
     }
 
     private static void replaceParam (final String userResourceParamName, final JsonElement userResourceParamValue,
-            final JsonObject template) {
+            final JsonObject template, List<JsonElement> parents) {
+        parents.add(template);
 
-        template.keySet().forEach(templateAttr -> {
+        new HashSet<>(template.keySet()).forEach(templateAttr -> {
             final JsonElement templateAttrValue = template.get(templateAttr);
-            if (isListObject(templateAttrValue)){
-                templateAttrValue.getAsJsonArray().forEach(element -> {
-                    redirectArrayElement(userResourceParamName, userResourceParamValue, templateAttr, element);
-                });
-            } else if (isObject(templateAttrValue)){
-                replaceParam(userResourceParamName, userResourceParamValue, templateAttrValue.getAsJsonObject());
+
+            if (templateAttrValue.isJsonArray()){
+                // if attribute is an array, should run each node
+                List<JsonElement> elements = StreamSupport.stream(templateAttrValue.getAsJsonArray().spliterator(), false).collect(Collectors.toList());
+                for (int i = elements.size(); i > 0; i--) {
+                    redirectArrayElement(userResourceParamName, userResourceParamValue, templateAttr, elements.get(i - 1), parents);
+                }
+
+            } else if (templateAttrValue.isJsonObject()){
+                // if attribute is an object, should run each sub attribute
+                replaceParam(userResourceParamName, userResourceParamValue, templateAttrValue.getAsJsonObject(), parents);
+
             } else {
-                replace(userResourceParamName, userResourceParamValue, templateAttr, templateAttrValue);
+                // so, verify if need replace
+                replace(userResourceParamName, userResourceParamValue, templateAttr, templateAttrValue, parents);
             }
         });
 
+        parents.remove(template);
     }
 
     private static void replace (final String userResourceParamName, final JsonElement userResourceParamValue,
-            final String templateAttr, final JsonElement templateAttrValue) {
+            final String templateAttr, final JsonElement templateAttrValue, List<JsonElement> parents) {
+
         if (isPrimitiveValidToReplace(userResourceParamName, userResourceParamValue, templateAttr, templateAttrValue)){
-            System.out.println("replace " + templateAttrValue + " to " + userResourceParamValue.getAsString());
+            JsonElement father = parents.get(parents.size() - 1);
+            JsonElement grandfather = parents.get(parents.size() - 2);
+
+            if (father.isJsonArray()){
+                if (grandfather.isJsonArray()){
+                    // is a father array and grandfather array
+                    System.out.println("father array and grandfather array");
+                } else {
+                    // is a father array and grandfather object
+                    System.out.println("father array and grandfather object");
+                }
+            } else {
+                if (grandfather.isJsonArray()){
+                    // is a father object and grandfather array
+                    System.out.println("father object and grandfather array");
+                } else {
+                    // is a father object and grandfather object
+
+                    if (isFatherInsideArray(father, grandfather)) {
+                        // replace value inside a grandfather value array
+                        grandfather.getAsJsonObject().entrySet().stream()
+                                .filter(set -> set.getValue().isJsonArray())
+                                .map(set -> set.getValue().getAsJsonArray())
+                                .filter(array -> StreamSupport.stream(array.spliterator(), false)
+                                        .anyMatch(e -> e.equals(father)))
+                                .forEach(array -> {
+                                    array.remove(father);
+                                    array.add(userResourceParamValue.getAsString());
+                                });
+                    } else {
+                        // replace value
+                        String fatherName = grandfather.getAsJsonObject().entrySet().stream()
+                                .filter(set -> set.getValue().equals(father))
+                                .findFirst()
+                                .map(Map.Entry::getKey)
+                                .orElseThrow(RuntimeException::new);
+                        grandfather.getAsJsonObject().remove(fatherName);
+                        grandfather.getAsJsonObject().addProperty(fatherName, userResourceParamValue.getAsString());
+                    }
+                }
+            }
         }
+    }
+
+    private static boolean isFatherInsideArray(JsonElement father, JsonElement grandfather) {
+        return grandfather.getAsJsonObject().entrySet().stream()
+                                .filter(set -> set.getValue().isJsonArray())
+                                .map(set -> set.getValue().getAsJsonArray())
+                                .anyMatch(array -> StreamSupport.stream(array.spliterator(), false)
+                                        .anyMatch(e -> e.equals(father)));
     }
 
     private static void redirectArrayElement (final String userResourceParamName,
             final JsonElement userResourceParamValue,
-            final String templateAttr, final JsonElement element) {
+            final String templateAttr, final JsonElement element, List<JsonElement> parents) {
         if (element.isJsonArray()){
-            element.getAsJsonArray().forEach(subElement -> {
-                redirectArrayElement(userResourceParamName, userResourceParamValue, templateAttr, subElement);
-            });
+            // if element is another array, should run each node
+            parents.add(element);
+            element.getAsJsonArray().forEach(subElement -> redirectArrayElement(userResourceParamName, userResourceParamValue, templateAttr, subElement, parents));
         } else if (element.isJsonObject()){
-            replaceParam(userResourceParamName, userResourceParamValue, element.getAsJsonObject());
+            // if element inside array is an object, should run each attribute
+            replaceParam(userResourceParamName, userResourceParamValue, element.getAsJsonObject(), parents);
         } else {
-            replace(userResourceParamName, userResourceParamValue, templateAttr, element);
+            // so, verify if need replace
+            replace(userResourceParamName, userResourceParamValue, templateAttr, element, parents);
         }
-    }
-
-    private static boolean isObject (final JsonElement templateAttrValue) {
-        return templateAttrValue.isJsonObject();
-    }
-
-    private static boolean isListObject (final JsonElement templateAttrValue) {
-        return templateAttrValue.isJsonArray();
     }
 
     private static boolean isPrimitiveValidToReplace (final String userResourceParamName,
